3aa33399bf9aa2ac6ce816be780a099dcf51983c
NPinzin <nick.pinzin@gmail.com>
2024-11-29 10:08:59 -0500

    Amplify and React App

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Table } from './components/Table/Table.js';
+
+function App() {
+  return (
+    <div>
+      <p>Hello</p>
+      <Table />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Table/Table.js
@@ -0,0 +1,7 @@
+import './table.css'
+
+export const Table = () => {
+  return (
+    <h1>Table component</h1>
+  )
+}
